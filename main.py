96d21cf6969fa6df70138c018dd2a6b3a015579a
Guimarret <guimarret@gmail.com>
2023-07-08 15:21:06 -0300

    repo verify

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("irra")
+import boto3
+
+def lamdba_handler(event, context):
+    pass
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -9,3 +9,4 @@ module "lambda_function_container_image" {
   image_uri    = "132367819851.dkr.ecr.eu-west-1.amazonaws.com/complete-cow:1.0"
   package_type = "Image"
 }
+
