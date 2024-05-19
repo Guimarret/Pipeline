265a94b4e7c114e460aba96b436e44141af85399
Guimarret <guimarret@gmail.com>
2024-05-19 00:30:02 -0300

    Lambda fix

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,12 +1,7 @@
-module "lambda_function_container_image" {
-  source = "terraform-aws-modules/lambda/aws"
-
-  function_name = "my-lambda-existing-package-local"
-  description   = "My awesome lambda function"
-
-  create_package = false
-
-  image_uri    = "132367819851.dkr.ecr.eu-west-1.amazonaws.com/complete-cow:1.0"
-  package_type = "Image"
+resource "aws_lambda_function" "lambda_pipeline_workflow" {
+  function_name = "lambda_pipeline_workflow"
+  timeout       = 300
+  image_uri     = data.aws_ecr_image.lamdba_image.image_uri
+  package_type  = "Image"
+  role          = aws_iam_role.lambda.arn
 }
-
