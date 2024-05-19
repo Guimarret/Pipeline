a0b49d1eba84b90f25b83ffaba0024c752e2d983
Guimarret <guimarret@gmail.com>
2024-05-19 00:23:55 -0300

    Basic file from scratch to develop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import boto3
-
-def lamdba_handler(event, context):
-    pass
+def lambda_handler(event, context):
+    # TODO: Add your logic here
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
